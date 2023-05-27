9b10e11483a0eff0f3277ea00efed7b17304e829
Abhijit Gayen <109941089+AbhijitCogoport@users.noreply.github.com>
2023-05-28 00:07:20 +0530

    day3_1:

---
+++ DAY3/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const {botResponse} = require('./chatbot/botResponse')
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.get('/bot_response', (req, res) =>{
+    console.log(req.query);
+    if (req.query?.message){
+        botResponse(req.query.message).then((response) =>{
+            res.send(response);
+        });
+    }
+    else{
+        res.send('No response sorry Not working')
+    }
+});
+
+app.post('/bot_response', (req, res) =>{
+    const user_message = req.query?.message;
+    if(user_message){
+        botResponse(req.query.message).then((response) =>{
+            res.send(response);
+        });
+    }
+    else{
+        res.send('No response sorry Not working')
+    }
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
